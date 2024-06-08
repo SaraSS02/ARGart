b8404f0e107c8af1e5024fe6bb3fa87bba45f841
SaraSS02 <s285050@studenti.polito.it>
2024-06-08 19:35:07 +0200

    klynt

--- data.js
+++ data.js
@@ -6,6 +6,48 @@ klynt.data = {
             "backgroundColor": "rgba(0,0,0,1)",
             "y": 151,
             "x": 401,
+            "shapes": [
+                {
+                    "id": "4FED99FC-38FA-AEFB-BC15-F387764832A8",
+                    "centerY": "209.20",
+                    "scales": true,
+                    "width": "316.39",
+                    "label": "Hotspot 1",
+                    "databegin": "00:00.00",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    },
+                    "centerX": "-2.94",
+                    "dataend": "00:16.06",
+                    "height": "72.85",
+                    "duration": "00:16.06",
+                    "zIndex": 1,
+                    "link": {
+                        "id": "E7FCE683-85F2-3C47-385C-F387C6F6DAE9",
+                        "linkType": "linkToSequence",
+                        "targetSequence": "D1AD29F4-EF73-2683-6B9F-F386EFB71BCC",
+                        "automaticTransition": false,
+                        "closeButtonTop": 16,
+                        "pauseParent": true,
+                        "automaticClose": false,
+                        "closeButton": true,
+                        "controlDX": 0,
+                        "hideInMindmap": false,
+                        "overlay": true,
+                        "controlDY": 0,
+                        "transition": "fade",
+                        "closeButtonRight": 16,
+                        "color": "rgba(0,0,0,1)",
+                        "hideArrowInMindmap": false
+                    },
+                    "type": "default",
+                    "style": {
+                        "backgroundColor": "rgba(233,66,77,1)",
+                        "opacity": 0
+                    }
+                }
+            ],
             "hideTitleInMindmap": false,
             "thumbnail": "Medias/Thumbnails/PHOTO-2024-05-21-13-17-32.jpg.jpg",
             "size": "medium",
@@ -14,28 +56,28 @@ klynt.data = {
             "videos": [
                 {
                     "player": false,
-                    "poster": "Medias/Posters/artisti.mp4.jpg",
+                    "id": "D09CEC37-519F-FDA5-1DA9-E9988231B298",
                     "src": [
                         {
                             "type": "video/mp4",
                             "src": "Medias/Videos/artisti.mp4"
                         }
                     ],
-                    "autoplay": true,
-                    "scales": true,
-                    "horizontalAlign": "center",
-                    "frameRate": 0,
-                    "loop": true,
-                    "id": "D09CEC37-519F-FDA5-1DA9-E9988231B298",
-                    "centerY": "14.77",
-                    "width": "1804.75",
-                    "name": "artisti.mp4",
-                    "databegin": "00:00.00",
                     "transitionIn": {
                         "type": "fade",
                         "duration": "1"
                     },
+                    "scales": true,
+                    "horizontalAlign": "center",
+                    "frameRate": 0,
+                    "loop": true,
+                    "centerY": "14.77",
+                    "width": "1804.75",
+                    "name": "artisti.mp4",
+                    "databegin": "00:00.00",
+                    "poster": "Medias/Posters/artisti.mp4.jpg",
                     "centerX": "5.78",
+                    "autoplay": true,
                     "dataend": "00:16.06",
                     "verticalAlign": "middle",
                     "mediaWidth": 970,
@@ -50,15 +92,49 @@ klynt.data = {
             "hideThumbnailInMindmap": false,
             "color": "rgba(0,0,0,1)"
         },
+        "D1AD29F4-EF73-2683-6B9F-F386EFB71BCC": {
+            "id": "D1AD29F4-EF73-2683-6B9F-F386EFB71BCC",
+            "title": "Sequence 2",
+            "backgroundColor": "rgba(0,0,0,0.75)",
+            "iframes": [
+                {
+                    "id": "5576185F-480D-883D-804A-F387051E5FDE",
+                    "centerY": "22.70",
+                    "scales": true,
+                    "width": "507.45",
+                    "databegin": "00:00.00",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    },
+                    "centerX": "4.58",
+                    "dataend": "00:29.44",
+                    "transitionOut": {
+                        "type": "fade",
+                        "duration": "1"
+                    },
+                    "height": "459.08",
+                    "duration": "00:29.44",
+                    "zIndex": 0,
+                    "style": {
+                        "rotation": -0.0017485284269804495
+                    },
+                    "code": "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/530PJ_ERiYI?si=BzlK0olCxHvrxZZ3\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen><!-- Nel codice HTML del widget -->\r<div class=\"responsive-iframe-container\">\r  <iframe src=\"URL_DEL_TUO_IFRAME\" frameborder=\"0\" allowfullscreen></iframe>\r</div>\r<style>\r  .responsive-iframe-container {\r    position: relative;\r    padding-bottom: 56.25%; /* 16:9 */\r    height: 0;\r    overflow: hidden;\r  }\r  \r  .responsive-iframe-container iframe {\r    position: absolute;\r    top: 0;\r    left: 0;\r    width: 100%;\r    height: 100%;\r  }\r</style>\r</iframe>"
+                }
+            ],
+            "y": 158,
+            "x": 246.98333333333335,
+            "hideTitleInMindmap": false,
+            "size": "small",
+            "hideInMindmap": false,
+            "duration": 30,
+            "hideThumbnailInMindmap": false,
+            "color": "rgba(0,0,0,1)"
+        },
         "BD451DB9-64DF-BE3F-E9BE-B96D39EB2C8D": {
             "id": "BD451DB9-64DF-BE3F-E9BE-B96D39EB2C8D",
             "title": "Home",
             "backgroundColor": "rgba(0,0,0,1)",
-            "y": 400,
-            "x": 639.9833333333333,
-            "hideTitleInMindmap": false,
-            "thumbnail": "Medias/Thumbnails/PHOTO-2024-05-21-13-17-32.jpg.jpg",
-            "size": "large",
             "iframes": [
                 {
                     "id": "296BD0CA-FE2F-D9E7-2B5D-B96F3150ADAE",
@@ -78,11 +154,16 @@ klynt.data = {
                     "code": "<!DOCTYPE html>\r<html>\r<head>\r\t<script src=\"https://cdn.jsdelivr.net/gh/Klynt/Klynt-Player/Player/js/api/KlyntAPI.js\" type=\"text/javascript\"></script>\r\r\t<style>\r\tbody {\r\t\theight: 100px;\r\t}\r\tinput {\r\t\twidth: 100%;\r\t\tborder-top-color: transparent;\r\t\tborder-left-color: transparent;\r\t\tborder-right-color: transparent;\r\t\tfont-size: 24px;\r\t}\r\tinput:focus{\r\t\toutline: none;\r\t}\r\r\tbutton {\r\t\tpadding: 8px;\r\t\tmargin-top: 16px;\r\t\tborder: none;\r\t\tbackground: #08457e;\r\t\tcolor: white;\r\t\twidth: 100%\r\t}\r\t</style>\r\r</head>\r\r<body>\r\r\t<form name=\"myForm\" action=\"javascript:void(0);\" onsubmit=\"validateForm()\">\r\t\t<input type=\"text\" name=\"keypass\" autocomplete=\"off\">\r\t</form>\r<button onclick=\"validateForm()\">Invia</button>\r\r\t<script>\r\tfunction validateForm() {\r\t\tvar x = document.forms[\"myForm\"][\"keypass\"].value;\r\t\tif (x == \"armoniadelsuono\") {\r\t\t\tKlyntAPI.commands.openSequence(\"BF8C9A17-513D-A21A-164D-B9763B0C5853\");\r\t\t} else if (x==\"\") {\r\t\t\talert(\"Inserire risposta\");\r\t\t}else {\r\t\t\talert(\"Password errata\");\r\t\t}\r\t}\r\t</script>\r</body>\r</html>"
                 }
             ],
+            "y": 400,
+            "x": 639.9833333333333,
+            "hideTitleInMindmap": false,
+            "thumbnail": "Medias/Thumbnails/PHOTO-2024-05-21-13-17-32.jpg.jpg",
+            "size": "large",
             "hideInMindmap": false,
             "duration": 15.06,
             "videos": [
                 {
-                    "poster": "Medias/Posters/liberArtisti.mp4.jpg",
+                    "height": "1007.71",
                     "src": [
                         {
                             "type": "video/mp4",
@@ -91,13 +172,12 @@ klynt.data = {
                     ],
                     "autoplay": true,
                     "scales": true,
-                    "height": "1007.71",
+                    "zIndex": 0,
                     "transitionIn": {
                         "type": "fade",
                         "duration": "1"
                     },
                     "centerX": "0.58",
-                    "zIndex": 0,
                     "syncMaster": true,
                     "id": "96D14802-3DE6-45BE-EBC6-E992EA3F2415",
                     "centerY": "17.80",
@@ -106,6 +186,7 @@ klynt.data = {
                     "horizontalAlign": "center",
                     "name": "liberArtisti.mp4",
                     "databegin": "00:00.00",
+                    "poster": "Medias/Posters/liberArtisti.mp4.jpg",
                     "width": "1796.87",
                     "dataend": "00:15.06",
                     "mediaWidth": 970,
@@ -121,10 +202,80 @@ klynt.data = {
             "color": "rgba(0,0,0,1)"
         }
     },
+    "share": {
+        "enableLinkedIn": true,
+        "link": null,
+        "embedWording": "Embed",
+        "enableTumblr": true,
+        "verticalWording": "Vertical",
+        "playerWording": "Player",
+        "title": "",
+        "horizontalWording": "Horizontal",
+        "customSizeWording": "Custom",
+        "originalSizeWording": "Original",
+        "shareSequenceWording": "Share sequence",
+        "linkWording": "Link",
+        "removeViaKlynt": true,
+        "shortMessage": "",
+        "displayMiniPlayerWording": true,
+        "socialNetworksWording": "Social Networks",
+        "miniPlayerWording": "Mini player",
+        "thumbnail": null,
+        "enableGooglePlus": true,
+        "message": ""
+    },
+    "miniPlayer": {
+        "fullscreenInfoWording": "This program will launch in fullscreen",
+        "title": "ARG_2",
+        "downloadAppWording": "Download App",
+        "description": "",
+        "yesWording": "Yes",
+        "redirectToMobileApp": "auto",
+        "thanksForWatchingWording": "Thanks for watching!",
+        "launchAppWording": "Then Launch Project",
+        "url": "",
+        "resumePlaybackWording": "Resume playback?",
+        "noWording": "No",
+        "thumbnail": "Medias/Thumbnails/PHOTO-2024-05-21-13-17-32.jpg.jpg",
+        "analyticsKey": ""
+    },
+    "mainSequence": "BD451DB9-64DF-BE3F-E9BE-B96D39EB2C8D",
+    "advanced": {
+        "enableFullWindowMode": true,
+        "enableKeyboardNavigation": true,
+        "enableScrollNavigation": true,
+        "hasRemoteVideos": false,
+        "remoteVideosAPILocalBitrate": "best",
+        "enableSwipeNavigation": true,
+        "remoteVideosAPIURL": "http://api.klynt.net/video/klynt-video-api.php"
+    },
+    "general": {
+        "ratio": "custom",
+        "username": "polito",
+        "title": "ARG_2",
+        "level": 5,
+        "primaryColor": 3355443,
+        "projectId": "9FE76BE7-D68A-39FC-E140-B96D39906105",
+        "description": "",
+        "author": "polito",
+        "projectHost": "",
+        "secondaryColor": 16777215,
+        "editorVersionLabel": "Klynt 3.5.007",
+        "creationDate": 1716802894225,
+        "analyticsKey": "",
+        "tertiaryColor": 15286861,
+        "width": 545,
+        "overlayCloseButtonRight": 16,
+        "mapsKey": "",
+        "mainFontName": "Open Sans",
+        "height": 970,
+        "modificationDate": 1717867942345,
+        "keywords": null,
+        "mainFontSize": 14,
+        "overlayCloseButtonTop": 16,
+        "editorVersionNumber": "3.5.007"
+    },
     "menu": {
-        "backgroundImage": null,
-        "offset_sequence": 12,
-        "searchWidget": "9288F767-12E2-BDBC-9526-B96D39DF666A",
         "widgets": [
             {
                 "params": {
@@ -133,7 +284,8 @@ klynt.data = {
                     "displayDuration": true,
                     "sequences": [
                         "BD451DB9-64DF-BE3F-E9BE-B96D39EB2C8D",
-                        "BF8C9A17-513D-A21A-164D-B9763B0C5853"
+                        "BF8C9A17-513D-A21A-164D-B9763B0C5853",
+                        "D1AD29F4-EF73-2683-6B9F-F386EFB71BCC"
                     ]
                 },
                 "id": "6E3FB96E-0A4A-1C1C-568D-B96D39E0DD82",
@@ -147,7 +299,7 @@ klynt.data = {
                     "subtitle": "",
                     "aboutColumnText": "",
                     "creditsColumnTitle": "Credits",
-                    "creditsColumnText": ""
+                    "creditsColumnText": "Progetto didattico per il corso di Transmedia all'interno del corso di laurea in Ingegneria del cinema e dei mezzi di comunicazione del Politecnico di Torino"
                 },
                 "id": "263E0C83-9FE6-22D9-DC6A-B96D39DF2AA6",
                 "type": "credits",
@@ -174,68 +326,10 @@ klynt.data = {
                 "label": "Mindmap"
             }
         ],
+        "backgroundImage": null,
+        "searchWidget": "9288F767-12E2-BDBC-9526-B96D39DF666A",
+        "offset_sequence": 12,
         "displayBackgroundImage": false
-    },
-    "miniPlayer": {
-        "fullscreenInfoWording": "This program will launch in fullscreen",
-        "title": "ARG_2",
-        "thanksForWatchingWording": "Thanks for watching!",
-        "description": "",
-        "resumePlaybackWording": "Resume playback?",
-        "downloadAppWording": "Download App",
-        "yesWording": "Yes",
-        "url": "",
-        "redirectToMobileApp": "auto",
-        "noWording": "No",
-        "thumbnail": "Medias/Thumbnails/PHOTO-2024-05-21-13-17-32.jpg.jpg",
-        "analyticsKey": "",
-        "launchAppWording": "Then Launch Project"
-    },
-    "mainSequence": "BD451DB9-64DF-BE3F-E9BE-B96D39EB2C8D",
-    "advanced": {
-        "enableFullWindowMode": true,
-        "enableKeyboardNavigation": true,
-        "enableScrollNavigation": true,
-        "hasRemoteVideos": false,
-        "remoteVideosAPILocalBitrate": "best",
-        "enableSwipeNavigation": true,
-        "remoteVideosAPIURL": "http://api.klynt.net/video/klynt-video-api.php"
-    },
-    "general": {
-        "ratio": "custom",
-        "username": "polito",
-        "title": "ARG_2",
-        "level": 5,
-        "projectId": "9FE76BE7-D68A-39FC-E140-B96D39906105",
-        "primaryColor": 3355443,
-        "description": "",
-        "author": "polito",
-        "projectHost": "",
-        "secondaryColor": 16777215,
-        "editorVersionLabel": "Klynt 3.5.007",
-        "creationDate": 1716802894225,
-        "analyticsKey": "",
-        "tertiaryColor": 15286861,
-        "width": 545,
-        "overlayCloseButtonRight": 16,
-        "mapsKey": "",
-        "mainFontName": "Open Sans",
-        "height": 970,
-        "modificationDate": 1717776010633,
-        "keywords": null,
-        "mainFontSize": 14,
-        "overlayCloseButtonTop": 16,
-        "editorVersionNumber": "3.5.007"
-    },
-    "aliases": {
-        "idToAlias": {
-            "BF8C9A17-513D-A21A-164D-B9763B0C5853": "Corretta",
-            "BD451DB9-64DF-BE3F-E9BE-B96D39EB2C8D": "Home"
-        },
-        "aliasToId": {
-            "Home": "BD451DB9-64DF-BE3F-E9BE-B96D39EB2C8D",
-            "Corretta": "BF8C9A17-513D-A21A-164D-B9763B0C5853"
-        }
     },
     "watermark": {
         "action": {
@@ -247,38 +341,21 @@ klynt.data = {
         "enabled": false,
         "position": "null"
     },
-    "share": {
-        "enableLinkedIn": true,
-        "link": null,
-        "originalSizeWording": "Original",
-        "enableTumblr": true,
-        "verticalWording": "Vertical",
-        "playerWording": "Player",
-        "title": "",
-        "embedWording": "Embed",
-        "customSizeWording": "Custom",
-        "miniPlayerWording": "Mini player",
-        "shareSequenceWording": "Share sequence",
-        "linkWording": "Link",
-        "removeViaKlynt": true,
-        "horizontalWording": "Horizontal",
-        "socialNetworksWording": "Social Networks",
-        "message": "",
-        "thumbnail": null,
-        "enableGooglePlus": true,
-        "shortMessage": "",
-        "displayMiniPlayerWording": true
+    "aliases": {
+        "aliasToId": {
+            "Corretta": "BF8C9A17-513D-A21A-164D-B9763B0C5853",
+            "Home": "BD451DB9-64DF-BE3F-E9BE-B96D39EB2C8D",
+            "Sequence_2": "D1AD29F4-EF73-2683-6B9F-F386EFB71BCC"
+        },
+        "idToAlias": {
+            "BF8C9A17-513D-A21A-164D-B9763B0C5853": "Corretta",
+            "D1AD29F4-EF73-2683-6B9F-F386EFB71BCC": "Sequence_2",
+            "BD451DB9-64DF-BE3F-E9BE-B96D39EB2C8D": "Home"
+        }
     },
     "footer": {
         "height": 26,
         "items": [
-            {
-                "action": {
-                    "type": "openWidget",
-                    "target": "6E3FB96E-0A4A-1C1C-568D-B96D39E0DD82"
-                },
-                "label": "Index"
-            },
             {
                 "action": {
                     "type": "openWidget",
@@ -294,16 +371,6 @@ klynt.data = {
         },
         "buttons": [
             {
-                "icon": "share",
-                "type": "share",
-                "label": "Share"
-            },
-            {
-                "icon": "sound",
-                "type": "sound",
-                "label": "Sound"
-            },
-            {
                 "icon": "fullscreen",
                 "type": "fullscreen",
                 "label": "Fullscreen"
@@ -318,8 +385,8 @@ klynt.data = {
     "wording": {
         "your_are_here": "Vous êtes ici",
         "total_duration": "(durée totale : {0})",
-        "total_sequences": "séquences au total",
         "program_percentage_viewed": "du programme vu",
+        "total_sequences": "séquences au total",
         "keywords": "mots-clés"
     }
 }
--- miniPlayerData.js
+++ miniPlayerData.js
@@ -1,15 +1,15 @@
 klynt.miniPlayerData = {
     "fullscreenInfoWording": "This program will launch in fullscreen",
     "title": "ARG_2",
-    "thanksForWatchingWording": "Thanks for watching!",
-    "description": "",
-    "resumePlaybackWording": "Resume playback?",
     "downloadAppWording": "Download App",
+    "description": "",
     "yesWording": "Yes",
-    "url": "",
     "redirectToMobileApp": "auto",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "launchAppWording": "Then Launch Project",
+    "url": "",
+    "resumePlaybackWording": "Resume playback?",
     "noWording": "No",
     "thumbnail": "Medias/Thumbnails/PHOTO-2024-05-21-13-17-32.jpg.jpg",
-    "analyticsKey": "",
-    "launchAppWording": "Then Launch Project"
+    "analyticsKey": ""
 }
